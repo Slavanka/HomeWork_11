95daf75d5e441638a171e0fd39d466a98663bec2
viktoria.klisheva <vikmah2@gmail.com>
2022-01-23 22:09:51 +0300

    .

--- src/test/java/guru.qa/tests/TestBase.java
+++ src/test/java/guru.qa/tests/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
 
         com.codeborne.selenide.Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/#//wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
