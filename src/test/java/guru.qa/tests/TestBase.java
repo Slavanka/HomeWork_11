98b796511e2713a0d1f4549c1764ef88ac636e78
viktoria.klisheva <vikmah2@gmail.com>
2022-02-10 02:31:16 +0300

    .

--- src/test/java/guru.qa/tests/TestBase.java
+++ src/test/java/guru.qa/tests/TestBase.java
@@ -25,6 +25,7 @@ public class TestBase {
         String user = System.getProperty("user");
         String password = System.getProperty("password");
         Configuration.remote = "https://" + user + ":" + password + "@" + remoteUrl;
+        Configuration.remote = System.getProperty ("remote_driver_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
 //        String login = System.getProperty("login","user1");
 //        String password = System.getProperty("password","1234");
